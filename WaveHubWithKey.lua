cfb6e36cd36f1249c42893b6925f6006ba0b0b26
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-06-23 00:22:25 +0200

    Update WaveHubWithKey.lua

--- WaveHubWithKey.lua
+++ WaveHubWithKey.lua
@@ -29,7 +29,7 @@ end
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Wave Hub"; -- the title (ofc)
-Text = "version 6.6"; -- what the text says (ofc)
+Text = "version 6.7"; -- what the text says (ofc)
 --Icon = "rbxassetid://57254792"; -- the image if u want. 
 Duration = 1; -- how long the notification should in secounds
 })
