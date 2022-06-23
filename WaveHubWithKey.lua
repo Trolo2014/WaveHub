f5aa479db006c71029aafb49c13a2384a6e9f4e1
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-06-23 12:31:48 +0200

    Update WaveHubWithKey.lua

--- WaveHubWithKey.lua
+++ WaveHubWithKey.lua
@@ -16,6 +16,23 @@ end)
 end
 ServerPorn()
  
+function ServerPorn()
+spawn(function()
+repeat wait() until game.CoreGui:FindFirstChild('RobloxPromptGui')
+ 
+local lp,po,ts = game:GetService('Players').LocalPlayer,game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService('TeleportService')
+ 
+po.ChildAdded:connect(function(a)
+    if a.Name == 'ErrorPrompt' then
+        repeat
+            ts:Teleport(game.PlaceId)
+            wait(2)
+        until false
+    end
+end)
+end)
+end
+ServerPorn()
 repeat wait() until game:IsLoaded()
 game.StarterGui:SetCore("SendNotification", {
 Title = "Wave Hub"; -- the title (ofc)
