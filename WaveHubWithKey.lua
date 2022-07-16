97bedfd0f5a2d0901de33e621bc75735b4646136
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-07-16 15:34:37 +0200

    Update WaveHubWithKey.lua

--- WaveHubWithKey.lua
+++ WaveHubWithKey.lua
@@ -18,7 +18,7 @@ repeat wait() until game:IsLoaded()
 
 game.StarterGui:SetCore("SendNotification", {
 Title = "Wave Hub"; -- the title (ofc)
-Text = "Version 9.9"; -- what the text says (ofc)
+Text = "Version 9.8"; -- what the text says (ofc)
 --Icon = "rbxassetid://57254792"; -- the image if u want. 
 Duration = 1; -- how long the notification should in secounds
 })
